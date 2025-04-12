e0ebb5841d7c00a7916618fe9b4f9c3b4b156c31
CarlosMinauro <136112385+CarlosMinauro@users.noreply.github.com>
2025-04-11 21:17:17 -0500

    Add files via upload

---
+++ SOLID (1)/IS2-S02-SOLID/src/main/java/pe/edu/ulima/sin/ocpv1/Figura.java
@@ -0,0 +1,5 @@
+package pe.edu.ulima.sin.ocpv1;
+
+public abstract class Figura {
+    public abstract double calcularArea();
+}
---
+++ SOLID (1)/IS2-S02-SOLID/src/main/java/pe/edu/ulima/sin/srpv1/EmailReporte.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.edu.ulima.sin.srpv1;
+
+/**
+ *
+ * @author l34210
+ */
+
+public class EmailReporte {
+
+    public void enviar(Reporte reporte, String email) {
+        System.out.println("Enviando reporte a " + email);
+        // Aquí iría la lógica real para enviar el correo
+    }
+}
